16ced02f1aa10aed179320809bf670162bb93452
Nikolaos <n@moraitak.is>
2018-02-23 22:37:58 +0100

    I think EM should work with the two extra columns

--- EM_imputation_analysis.R
+++ EM_imputation_analysis.R
@@ -1,4 +1,4 @@
-other_credit <- read.csv("EM_data.csv")
+other_credit <- read.csv("EM_data_fuller.csv")
 par(mfrow=c(2,3))
 hist(other_credit$WHISTLE_BLOWER_POLICY , xlab = "whistle blower policy") 
 hist(other_credit$ETHICS_POLICY , xlab="ethics policy")
@@ -9,3 +9,6 @@ no_na_credit <- credit %>% drop_na
 hist(as.integer(no_na_credit$WHISTLE_BLOWER_POLICY) - 1, xlab = "whistle blower policy")
 hist(as.integer(no_na_credit$ETHICS_POLICY) - 1,  xlab="ethics policy")
 hist(as.integer(no_na_credit$BRIBERY_POLICY) - 1, xlab="bribery policy")
+
+# These indices are most deviant
+# Int64Index([3132, 3068, 2937, 227, 331], dtype='int64')
